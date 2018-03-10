c644e81bd742cfc33170f5d536dee72147395d96
saraansh <bsaraansh@gmail.com>
2018-03-11 03:30:26 +0530

    user_list generated

--- user_extractor.py
+++ user_extractor.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Updated on Sun Mar 11 02:53:02 2017
+Updated on Sun Mar 11 03:27:44 2017
 
 @author: Hopeless
 """
 
-from urllib.request import urlopen
-from bs4 import BeautifulSoup as bs
 import lxml.html
 
 # Set the parameters to load user list
@@ -16,13 +14,29 @@ para1 = "min_races=100&min_texts=100"
 para2 = "&sort=wpm_textbests"
 para3 = "&rank_start=1&rank_end=100"
 
-# Load the page and extract profile links
+# Load the page and extract all links
 
 page = header + para1 + para2 + para3
 parsed_page = lxml.html.parse(page)
-links = parsed_page.xpath("//a/@href")
+links = parsed_page.xpath("//a/@href");
 
-# Print the links
+# Extract usernames and profile links
 
+users = ['saraansh']
+user_links = ['/profile?username=saraansh']
 for link in links:
-	print(link)
+	if(link[0:9]=='/profile?'):
+		users.append(link[18:])
+		user_links.append(link)
+
+# Saving usernames to user_list
+
+with open('user_list.txt','w+') as f:
+	for user in users:
+		f.write(user + "\n")
+
+"""
+Usernames or profile links can be used
+interchangeably to generate each other.
+Pick either to be saved for future use.
+""" 
