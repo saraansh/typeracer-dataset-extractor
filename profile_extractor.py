b2b31b6450d9a14ee86b7ad252a5fc6f3ec82544
saraansh <bsaraansh@gmail.com>
2018-03-11 15:03:23 +0530

    Profile data exported

--- profile_extractor.py
+++ profile_extractor.py
@@ -1,17 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar 11 03:32:18 2018
+Updated on Sun Mar 11 13:53:37 2018
 
 @author: Hopeless
 """
 
+import pandas as pd
+import numpy as np
 import lxml.html
-import time
+#import time
 
 # Function to save user profile
 def save_csv(user,link):
-	with open(user+".csv") as f:
-		dfs = []
+	# Extract profile data
+	page = lxml.html.parse(link)
+	tables = page.xpath('//table')
+	# Extract user stats
+	content = tables[2].xpath('.//tr/td//text()')
+	rows = int(len(content)/6)
+	stats = np.array(content).reshape(rows,6)
+	# Export profile data
+	df = pd.DataFrame(stats)
+	df.to_csv(user + ".csv", header=None, index=None)
 
 # Function to set parameters
 def set_parameters(user):
@@ -19,7 +29,8 @@ def set_parameters(user):
 	para1 = "username=" + user
 	# Extract number of races
 	tags = lxml.html.parse(header+para1).xpath("//td")
-	para2 = "&last=" + tags[0].text_content()
+	races = tags[0].text_content()
+	para2 = "&last=" + races
 	# Return the new link
 	return (header + para1 + para2)
 
@@ -36,5 +47,6 @@ for user in users:
 	link = set_parameters(user)
 	print ("Received Link -> " + link + "\n")
 	#time.sleep(1)
-	#save_csv(user,link)
-	#print (user+".csv saved!" + "\n\n")
+	save_csv(user,link)
+	print (user+".csv saved!" + "\n\n")
+	#time.sleep(2)
